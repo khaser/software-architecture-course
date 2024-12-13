4d9ceb8b2459f2d8094d2cb34f06e232b09a43ef
Kirill Mitkin <kirill.mitkin.03@gmail.com>
2024-12-14 00:02:06 +0300

    get rid of MapBuilder

--- practice-2-roguelike/src/main/kotlin/ru/mkn/krogue/model/map/Map.kt
+++ practice-2-roguelike/src/main/kotlin/ru/mkn/krogue/model/map/Map.kt
@@ -1,10 +1,30 @@
 package ru.mkn.krogue.model.map
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import ru.mkn.krogue.model.Item
 import ru.mkn.krogue.model.Position
-import kotlin.collections.Map
+import java.io.File
+import kotlin.collections.Map as HashMap
 
 @Serializable
-data class Map(val tiles: Map<Position, Tile>, val items: Map<Position, List<Item>>) {
+data class Map(val tiles: HashMap<Position, Tile>, val items: HashMap<Position, List<Item>>) {
+
+    fun saveToFile(filename: String) {
+        val json = Json.encodeToString(this)
+        File(filename).printWriter().use { out ->
+            out.println(json)
+        }
+    }
+
+    companion object {
+        fun loadFromFile(filename: String): Map {
+            TODO("unimplemented")
+        }
+
+        fun generate(width: Int, height: Int): Map {
+            TODO("unimplemented")
+        }
+    }
 }
--- practice-2-roguelike/src/main/kotlin/ru/mkn/krogue/model/map/MapBuilder.kt
+++
@@ -1,6 +0,0 @@
-package ru.mkn.krogue.model.map
-
-class MapBuilder {
-    fun loadFile(filename: String): Map = TODO()
-    fun generate(width: Int, height: Int): Map = TODO()
-}
